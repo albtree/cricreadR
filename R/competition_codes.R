2a82da2d1112f7c34c15dd31fb70a571de055282
albtree <88771954+albtree@users.noreply.github.com>
2024-05-15 22:34:18 +1000

    change to github address

--- R/competition_codes.R
+++ R/competition_codes.R
@@ -1,7 +1,13 @@
 
 competition_codes <- function(){
+<<<<<<< Updated upstream
 suppressPackageStartupMessages(library(tidyverse))
 competition_codes_df <- read.csv("data/comp_codes.csv") |>
+=======
+  suppressPackageStartupMessages(library(tidyverse))
+
+competition_codes_df <- read.csv("https://github.com/albtree/cricreadR/raw/main/data/comp_codes.csv") |>
+>>>>>>> Stashed changes
   arrange(competition_code, -season)
   return(competition_codes_df)
 }
