31e57800ec2d7e395983bfc9db385b9d679c110f
Tim <88771954+albtree@users.noreply.github.com>
2024-05-15 22:52:07 +1000

    Update load_bbb_data.R

--- R/load_bbb_data.R
+++ R/load_bbb_data.R
@@ -9,10 +9,10 @@ load_bbb_data <- function(comp_code = NULL,
     stop("At least the argument (competition_code) must be provided. Please run the function cricscrapR::competition_codes() for valid competition_codes")
   }
 
-  details <- read.csv("data/comp_codes.csv") |>
+  details <- read.csv("https://github.com/albtree/cricreadR/raw/main/data/comp_codes.csv") |>
     filter(competition_code == comp_code) |>
     slice_head(n = 1)
-  data <- readRDS(glue("data/{details$competition_code}_{details$gender}_ball_by_ball.rds"))#
+  data <- readRDS(url(glue("https://github.com/albtree/cricreadR/raw/main/data/{details$competition_code}_{details$gender}_ball_by_ball.rds"))#
 
   # Create a logical condition based on provided arguments
   condition <- TRUE
