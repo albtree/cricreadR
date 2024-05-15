792e6efc3dde1cd8d9880a93dd24156078b539f2
Tim <88771954+albtree@users.noreply.github.com>
2024-05-15 22:16:45 +1000

    Update competition_codes.R

--- R/competition_codes.R
+++ R/competition_codes.R
@@ -1,6 +1,6 @@
 
 competition_codes <- function(){
-
+suppressPackageStartupMessages(library(dplyr))
 competition_codes_df <- read.csv("data/comp_codes.csv") |>
   arrange(competition_code, -season)
   return(competition_codes_df)
